f231ae7ac934df4a35c38f04eb58844a1e6ab6a3
Dvour31 <dennyyudhaa@gmail.com>
2026-09-05 20:12:09 +0900

    Reorder playlist: move Dia Masa Lalumu to the end

--- data.js
+++ data.js
@@ -67,10 +67,10 @@ window.SITE_DATA = {
   // "src" ke file mp3 asli di assets/audio/
   playlist: [
     { title: "Shape of My Heart", artist: "Fagtos Fam", src: "assets/audio/shape.mp3" },
-    { title: "Dia Masa Lalumu, Aku Masa Depanmu", artist: "Fagtos Fam", src: "assets/audio/dia.mp3" },
     { title: "Astaga Bersyanda", artist: "Fagtos Fam", src: "assets/audio/astaga.mp3" },
     { title: "Menungso Ratoto", artist: "Fagtos Fam", src: "assets/audio/menungso.mp3" },
     { title: "Playdate", artist: "Fagtos Fam", src: "assets/audio/playdate.mp3" },
+    { title: "Dia Masa Lalumu, Aku Masa Depanmu", artist: "Fagtos Fam", src: "assets/audio/dia.mp3" },
   ],
 
   // ---- BRAND -----------------------------------------------------
